0ea285f0533b9f138d2fd5bfe8560efd183153f7
senolakkas <senolakkas@gmail.com>
2016-09-12 13:51:18 +0300

    modified:   components/App.js

--- components/App.js
+++ components/App.js
@@ -2,36 +2,46 @@ import React, { Component } from 'react';
 import Board from './Board'
 import {
   StyleSheet,
-  Text,  
+  Text,
   TextInput,
-  TouchableHighlight,  
-  View  
+  TouchableHighlight,
+  View
 } from 'react-native';
 import styles from './styles'
 
 
 export default class App extends Component {
+  getInitialState() {
+		return {
+			height: 10,
+			width: 10,
+			numMines: 5,
+			gameStarted: false
+		}
+	}
   renderBoard() {
     return (
       <Board />
     );
-  }  
+  }
   renderNewGameControls() {
     return (
       <View style={styles.container}>
         <Text>
           Width:
         </Text>
-        <TextInput style={{width: 100, height:40}} /> 
+        <TextInput keyboardType="numeric" value={this.state.width.toString()} style={{width: 100, height:40}} onChangeText={ (width) => this.setState({width}) } />
+
         <Text>
           Height:
         </Text>
-        <TextInput style={{width: 100, height:40}} />
+        <TextInput keyboardType="numeric" value={this.state.height.toString()} style={{width: 100, height:40}} onChangeText={ (height) => this.setState({height}) } />
+
 
         <Text>Num Mines:</Text>
-        <TextInput style={{width: 100, height:40}} />
+        <TextInput keyboardType="numeric" value={this.state.numMines.toString()} style={{width: 100, height:40}} onChangeText={ (numMines) => this.setState({numMines}) } />
 
-        <TouchableHighlight>
+        <TouchableHighlight onPress={this.startNewGame}>
         	<Text>Start</Text>
         </TouchableHighlight>
       </View>
@@ -40,7 +50,7 @@ export default class App extends Component {
   render() {
     if (true) {
       return this.renderNewGameControls();
-    }    
+    }
   }
-  
+
 }
