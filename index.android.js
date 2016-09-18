2d6538009e2b6bc7945f7946060e8d669ea8e2d9
senolakkas <senolakkas@gmail.com>
2016-09-18 23:09:38 +0300

    modified:   index.android.js

--- index.android.js
+++ index.android.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './components/App'
 import {
   AppRegistry
