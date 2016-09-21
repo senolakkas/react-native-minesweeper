2962ef3dc41ae0b743d60f18abdb7ef3287ebe04
senolakkas <senolakkas@gmail.com>
2016-09-21 12:25:55 +0300

    modified:   containers/app.js added redux-logger  	modified:   package.json added redux-logger

--- containers/app.js
+++ containers/app.js
@@ -2,14 +2,17 @@ import React, {Component} from 'react';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+
 
 import * as reducers from '../reducers';
 //import CounterApp from './counterApp';
 import StartNewGame from './startNewGame';
 
 
-
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(thunk, promise, logger)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
 
